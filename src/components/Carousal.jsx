16525d4ef9c677dcd265d62629b8884359c85320
Ashita Dashottar <ashita@Ashitas-MacBook-Pro.local>
2020-03-05 15:14:48 -0500

    links updated

--- src/components/Carousal.jsx
+++ src/components/Carousal.jsx
@@ -14,7 +14,7 @@ class Carousal extends React.Component{
                 {
                     id: 0,
                     title: 'AskHusky',
-                    subTitle: 'A MEAN-Stack Project, a peer-to-peer problem-solving platform where one can ask queries regarding academic subjects or otherwise. Built for Northeastern students.',
+                    subTitle: 'A MEAN-Stack Project, a peer-to-peer problem-solving platform where one can ask queries regarding academic subjects or otherwise. Built for Northeastern students',
                     imgSrc: AskHusky,
                     link: 'https://github.com/dashottar6/AskHusky',
                     selected: false
@@ -22,7 +22,7 @@ class Carousal extends React.Component{
                 {
                     id: 1,
                     title: 'Outreach Portal',
-                    subTitle: 'A PHP based application developed for the conduction of workshops between colleges in India. It is also used for database management and analytics of the same.',
+                    subTitle: 'A PHP based application developed for the conduction of workshops between colleges in India. It is also used for database management and analytics of the same',
                     imgSrc: Outreach,
                     link: 'http://vlabs.iitb.ac.in/outreachportal/',
                     selected: false
@@ -30,7 +30,7 @@ class Carousal extends React.Component{
                 {
                     id: 2,
                     title: 'Quarter Allocation System',
-                    subTitle: 'A PHP Project which helped Indian Railways, Mumbai(IN) to digitally allot houses to government officials. This came as a big relief from the manual process.',
+                    subTitle: 'A PHP Project which helped Indian Railways, Mumbai(IN) to digitally allot residence to government officials. This came as a big relief from the manual process',
                     imgSrc: train,
                     link: 'https://github.com/dashottar6/QuarterAllocationSystem',
                     selected: false
